31428b59c8cf9e7d59a1df14931515f10ad8c0d0
Subrahmanyam <77051669+subbu-techfix@users.noreply.github.com>
2021-08-02 16:34:32 +0530

    Delete Sentrifugo_3.2 directory

--- Sentrifugo_3.2/public/mail_settings_constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-	           defined('MAIL_SMTP') || define('MAIL_SMTP','smtp.gmail.com');
-	           defined('MAIL_USERNAME') || define('MAIL_USERNAME','subhash.175@gmail.com');
-	           defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','vmlrhsqkudmzedmj');
-	           defined('MAIL_PORT') || define('MAIL_PORT','587');
-	           defined('MAIL_AUTH') || define('MAIL_AUTH','true');
-	           defined('MAIL_TLS') || define('MAIL_TLS','');
-	         ?>
